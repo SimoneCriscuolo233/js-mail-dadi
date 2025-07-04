217e07e52876b3fdeea2a9a5823f2a39bb850e2f
SimoneCriscuolo233 <simone.criscuolo26052001@gmail.com>
2025-07-04 14:21:23 +0200

    feat: created a loop to check for the presence of userMail in guests and I print the result.
    To verify the presence of the email I created a variable before the loop that assigned the value of false and that only if the userMail coincides with one present in guests is changed to true.

--- js/script.js
+++ js/script.js
@@ -5,3 +5,17 @@ const guests = [`mario.bros@gmail.com`, `luigi.bros@libero.it`, `princess.peach@
 console.log(guests)
 //definisco una variabile per l`inserimento di una mail da parte dell'utente 
 const userMail = prompt(`Inserisci la tua mail`);
+//definisco una variabile di base falsa che viene modificata solo nel caso ci sia una mail gia' presente in guests 
+let found = false;
+//Creo un ciclo per verificare che la mail inserita  dall'utente sia presente in guests
+for (let i = 0; i < guests.length && !found; i++) {
+  if (guests[i] === userMail) {
+    found = true;
+  }
+}
+//Stampo l'esito
+if (found) {
+  console.log(`Sei nella lista degli invitati`)
+} else {
+  console.log(`Non sei nella lista degli invitati`)
+}
