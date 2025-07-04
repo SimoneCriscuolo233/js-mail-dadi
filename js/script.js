13a2b3adc302fb644745b82228a9353aaa261954
SimoneCriscuolo233 <simone.criscuolo26052001@gmail.com>
2025-07-04 13:36:20 +0200

    feat:  add an array with the emails of the guests and an input for the user's email

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//-----MAIL-----
+
+//definisco un array contenente le mail delle persone invitate
+const guests = [`mario.bros@gmail.com`, `luigi.bros@libero.it`, `princess.peach@gmail.com`, `princess.daisy@yahoo.it`, `toad.mushroom@alice.it`];
+console.log(guests)
+//definisco una variabile per l`inserimento di una mail da parte dell'utente 
+const ToVerify = prompt(`Inserisci la tua mail`);
